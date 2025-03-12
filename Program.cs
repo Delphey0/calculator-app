3e7660cd56281c1edd4dfae8bc5cc1138f671878
delphey <llolololol395@gmail.com>
2025-03-12 14:08:44 +0700

    Kode untuk Penambahan dan Pengurangan

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
-﻿class calculator {
-    static void Main(string[] args) {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+class Calculator {
+    static void Main(string[] args) {
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a,b));
+        Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a,b));
+
+        Console.WriteLine("\nTekan sembarang key untuk keluar");
+        Console.ReadKey();
+    }
+    static int Penambahan(int a, int b) { 
+        return a + b;
+    }
+    static int Pengurangan(int a, int b)
+    {
+        return a - b;
     }
 }
